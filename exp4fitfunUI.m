e4f4acdb562a7f20695d26cdc61ae038e258ff36
pc-vision <dmytro.lotnyk@gmail.com>
2018-10-16 00:06:23 -0400

    add beginning of the pulse. fit4 fits by 2 taus

--- exp4fitfunUI.m
+++ exp4fitfunUI.m
@@ -5,11 +5,14 @@ A=real(parameter(1));
 B=real(parameter(2));
 tau=real(parameter(3));
 t0=real(parameter(4));
-gamma=real(parameter(5));
+tau1=real(parameter(5));
+t1=real(parameter(6));
+%gamma=real(parameter(5));
 
 cv=zeros(1,length(t));
 for ii=1:length(t)
    %cv(1,ii)=A+B*t(ii)+exp(-(t(ii)-t0)/(tau+gamma*exp(-lambda*t(ii)))); 
    %cv(1,ii)=A+B*t(ii)+exp(-(t(ii)+t0)/(tau+gamma*t(ii)));
-   cv(1,ii)=A+B*t(ii)+0.5*(gamma+sqrt(gamma^2+4*exp(-(t(ii)+t0)/tau)));
+   %cv(1,ii)=A+B*t(ii)+0.5*(gamma+sqrt(gamma^2+4*exp(-(t(ii)+t0)/tau)));
+   cv(1,ii)=A+B*t(ii)+exp(-(t(ii)+t0)/tau)+exp(-(t(ii)+t1)/tau1);
 end
